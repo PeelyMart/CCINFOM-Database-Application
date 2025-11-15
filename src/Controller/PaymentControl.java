20da195a586063d0d97326b8b3b8d7aad23117cb
Gian <gian_enriquez@dlsu.edu.ph>
2025-11-15 11:25:30 +0800

    Payment Logic Update (Complete?)

--- src/Controller/PaymentControl.java
+++ src/Controller/PaymentControl.java
@@ -1,11 +1,13 @@
 package Controller;
 
+import DAO.PaymentDAO;
 import DAO.LoyaltymemberDAO;
 import Model.Order;
 import Model.Payment;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.time.LocalDateTime;
 
 public class PaymentControl {
     /**
@@ -13,11 +15,27 @@ public class PaymentControl {
      * @return 1 - successful, 0 - unsuccessful, -1 - the bill was 0.00 early return
      */
     public static int initiatePayment(Order currentOrder, int customerId){
-        if(currentOrder.getTotalCost().equals(BigDecimal.valueOf(0.00))) {return -1;}
+        if(currentOrder.getTotalCost().equals(BigDecimal.valueOf(0.00))) {
+            return -1;
+        }
 
         //creating the record for the payment
         Payment currPay = new Payment();
-        //TODO: PaymentDAO.recordPayment();
+        currPay.setOrderId(currentOrder.getOrderId());
+        currPay.setAmountPaid(currentOrder.getTotalCost().doubleValue());
+        currPay.setPaymentMethod(currentOrder.getPaymentMethod());
+        currPay.setPaymentDate(LocalDateTime.now());
+        currPay.setStaffId(currentOrder.getStaffId());
+        currPay.setLoyalCustomerId(customerId);
+        currPay.setActive(true);
+        
+        PaymentDAO paymentDAO = new PaymentDAO();
+        boolean success = paymentDAO.recordPayment(currPay);
+
+        if (!success) {
+            return 0;
+        }
+
         //reflection of points
         int points = calculateLoyaltyPoints(currentOrder.getTotalCost());
         LoyaltymemberDAO.addPoints(customerId, points);
@@ -30,11 +48,5 @@ public class PaymentControl {
         }
         return totalSpent.divide(BigDecimal.valueOf(1000), 0, RoundingMode.DOWN).intValue();
     }
-
-    public static void uploadPaymentInfo(){
-
-    }
-
-
 
 }
